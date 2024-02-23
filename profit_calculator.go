8fba5231bdcda049d6fac73060f0b058c80e9ffa
Bogach <roman.bogach@gmail.com>
2024-02-23 23:36:43 +0200

    Added functions

--- profit_calculator.go
+++ profit_calculator.go
@@ -5,21 +5,60 @@ import "fmt"
 func main() {
 	var revenue float64
 	var expenses float64
-	var taxRate float64 = 30.0
+	var taxRate float64
+	var earningBeforeTax float64
+	var profit float64
 
+	revenue = inputRevenue()
+	expenses = inputExpenses()
+	taxRate = inputTaxRate()
+	earningBeforeTax = earningBeforeTaxF(revenue, expenses)
+	profit = profitF(earningBeforeTax, taxRate)
+
+	println(earningBeforeTax)
+	println(profit)
+	println(ratioF(earningBeforeTax, profit))
+
+}
+
+func inputRevenue() float64 {
+	var revenue float64
 	fmt.Print("Please input revenue")
 	fmt.Scan(&revenue)
+	return revenue
+}
+
+func inputExpenses() float64 {
+	var expenses float64
 	fmt.Print("also ur expenses")
 	fmt.Scan(&expenses)
+	return expenses
+}
 
+func inputTaxRate() float64 {
+	var taxRate float64
+	fmt.Print("also ur TAXXX")
+	fmt.Scan(&taxRate)
+	return taxRate
+}
+
+func earningBeforeTaxF(revenue float64, expenses float64) float64 {
 	earningBeforeTax := revenue - expenses
-	profit := float64(earningBeforeTax) - taxRate
-	ratio := float64(earningBeforeTax) / profit
 	println("Earnings before Tax")
-	println(earningBeforeTax)
+	return earningBeforeTax
+
+}
+
+func profitF(earningBeforeTax float64, taxRate float64) float64 {
+	profit := float64(earningBeforeTax) - taxRate
 	println("Profit -")
-	println(profit)
+	return profit
+}
+
+func ratioF(earningBeforeTax float64, profit float64) float64 {
+	ratio := float64(earningBeforeTax) / profit
+
 	println("Ratio is")
-	println(ratio)
+	return ratio
 
 }
