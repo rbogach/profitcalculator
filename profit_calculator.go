41167427530aae85fff25efc3cf9a8f8f0f1ac57
Bogach <roman.bogach@gmail.com>
2024-03-09 14:27:26 +0200

    Added a function writing into file three variables separately added error handling

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,6 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const resultsFile = "results.txt"
+
+func writeResultToFile(ebt, profprofit, ratio float64) {
+	data := fmt.Sprintf("%sebt\n%d profit\n%s ratio\n", ebt, profprofit, ratio)
+
+	os.WriteFile(resultsFile, []byte(data), 0644)
+
+}
+
+func getResults() (float64, error) {
+	data, err := os.ReadFile(resultsFile)
+	if err != nil {
+		return 1000, errors.New("failed to find results file")
+
+	}
+	resultsText := string(data)
+	results, err := strconv.ParseFloat(resultsText, 64)
+	if err != nil {
+		return 1000, errors.New("failed to parce stored results value")
+	}
+	return results, nil
+}
 
 func main() {
 
@@ -13,27 +41,55 @@ func main() {
 	fmt.Printf("EBT"+"%.1f\n", ebt)
 	fmt.Printf("Porfit before Tax"+"%.1f\n", profprofit)
 	fmt.Printf("Ratio"+"%.1f\n", ratio)
+	writeResultToFile(ebt, profprofit, ratio)
+	getResults()
 
 }
 
 func inputRevenue() float64 {
 	var revenue float64
-	fmt.Print("Please input revenue")
-	fmt.Scan(&revenue)
+	for {
+		fmt.Print("Please input revenue")
+		fmt.Scan(&revenue)
+		if revenue > 0 {
+			//fmt.Println("Invalid amount")
+			break
+		}
+		fmt.Println("Invalid amount. Revenue must be greater than 0.")
+	}
+
 	return revenue
 }
 
 func inputExpenses() float64 {
 	var expenses float64
-	fmt.Print("also ur expenses")
-	fmt.Scan(&expenses)
+	for {
+		fmt.Print("also ur expenses")
+		fmt.Scan(&expenses)
+
+		if expenses > 0 {
+			///fmt.Println("Invalid expenses")
+			break
+
+		}
+		fmt.Println("Invalid amount. Revenue must be greater than 0.")
+
+	}
 	return expenses
 }
 
 func inputTaxRate() float64 {
 	var taxRate float64
-	fmt.Print("also ur TAXXX")
-	fmt.Scan(&taxRate)
+	for {
+		fmt.Print("also ur TAXXX")
+		fmt.Scan(&taxRate)
+		if taxRate > 0 {
+			//fmt.Println("Invalid Tax rate")
+			break
+		}
+		fmt.Println("Invalid amount. Revenue must be greater than 0.")
+	}
+
 	return taxRate
 }
 
